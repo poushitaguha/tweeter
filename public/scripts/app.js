08f049f615e7c85ae4e17c1a7ad4af2725c9b38a
poushitaguha <pguha.kolkata@gmail.com>
2019-06-11 17:57:00 +0000

    Implement character counter in tweeter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// function buttonClick(ev) => ();
+
+//  $button.addEventListener('click', buttonClick);
+
+$(document).ready(function() {
+    // console.log("Hello");
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+//Function to count characters in the tweet.
+//The code will update the character count dynamically and will turn the counter to red once the
+// tweet length becomes more than 140 characters.
+
+$(document).ready(function() {
+    $("textarea").on('input', function(event) {
+        let tweetLength = $(this).val().length;
+        // Check if tweet length becomes greater than 140
+        let remLength = 140 - tweetLength;
+        // Update the counter in tweeter page
+        $(".counter").text(remLength);
+        //Set the character counter to red if it goes below 0 by adding a class
+        if (remLength < 0) {
+            $(".counter").text(remLength).addClass("setRed");
+        } else {
+            $(".counter").text(remLength).removeClass("setRed");
+        }        
+    });
+  });
