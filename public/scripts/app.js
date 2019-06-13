d1fbecddb2a4101a8aa3d03bce2db70f5256b1f2
poushitaguha <pguha.kolkata@gmail.com>
2019-06-13 02:25:27 +0000

    Add code for form toggle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,54 +6,35 @@
 
 $(document).ready(function() {
 
-// Fake data taken from tweets.json
-// const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": {
-//           "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//           "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//           "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//         },
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": {
-//           "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//           "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//           "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//         },
-//         "handle": "@rd" },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     },
-//     {
-//       "user": {
-//         "name": "Johann von Goethe",
-//         "avatars": {
-//           "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//           "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//           "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//         },
-//         "handle": "@johann49"
-//       },
-//       "content": {
-//         "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//       },
-//       "created_at": 1461113796368
-//     }
-//   ];
+  // Loads tweet function which executes a GET request to load tweets
+    function loadTweets () {
+        $.ajax({
+            method: 'GET',
+            url: "/tweets",
+            dataType: 'JSON',
+            success: data => {
+                renderTweets(data);
+            }        
+        })
+    };
 
+    // Loops through tweets and calls createTweetElement for each tweet.
+    // Then takes return value and appends it to the tweets container   
+    function renderTweets(tweets) {   
+        tweets.forEach(function(item) {
+            var $tweet = createTweetElement(item);
+            $('.all-tweets').prepend($tweet);
+        });
+    };
+
+    // Function to handle cross-site scripting
+    function escape(str) {
+        var div = document.createElement('div');
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
+    };
+
+    // Function to create the tweet container dynamically and return new tweet
   function createTweetElement(data) {
 
     let tweetImage = data.user.avatars.small;
@@ -86,60 +67,40 @@ $(document).ready(function() {
     return HTMLToAppend;
   }  
 
-  function escape(str) {
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  }
+  // Code to toogle form
+  $('#compose-button').on('click', function (event) {
+    $('.new-tweet').slideToggle(100);
+    $('textarea').select();
+  })
 
-  function renderTweets(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container      
-    tweets.forEach(function(item) {
-        var $tweet = createTweetElement(item);
-        $('.all-tweets').prepend($tweet);
-    });
-  }
-  
-//   renderTweets(data);
+  loadTweets();
 
-function loadTweets () {
-    $.ajax({
-        method: 'GET',
-        url: "/tweets",
-        dataType: 'JSON',
-        success: data => {
-            renderTweets(data);
-        }        
-        })
-    }
-
-  // Following code submits a request to the server using JQuery
-  $("#tweetform").on('submit', function(event) {
-    event.preventDefault();
-    const $newTweet = $(this).serialize();
-    let tweet = $('textarea').val();
-    let tweetLength = $('textarea').val().length;
-    if (!tweet) {
-        alert ("Tweet not entered by the user !!");
-    } else if (tweetLength > 141) {
-        alert ("Tweet longer than 140 characters!!");
-    } else {
-        $.ajax({
-            method: 'POST',
-            url: "/tweets",
-            dataType: 'JSON',
-            data: $(this).serialize(),
-            success: function() {
-                loadTweets();
-            }
+    // Following code submits a request to the server using JQuery
+    // It also checks for the validity of the tweet and performs the POST only if
+    // the tweet is valid and length is greater than 140 characters.
+    $("#tweetform").on('submit', function(event) {
+        event.preventDefault();
+        let tweet = $('textarea').val();
+        let tweetLength = $('textarea').val().length;
+        if (!tweet) {
+            alert ("Tweet not entered by the user !!");
+        } else if (tweetLength > 141) {
+            alert ("Tweet longer than 140 characters!!");
+        } else {
+            $.ajax({
+                method: 'POST',
+                url: "/tweets",
+                dataType: 'JSON',
+                data: $(this).serialize(),
+                success: function() {
+                    loadTweets();
+                    // Refresh text area and reset counter
+                    $("textarea").val("");
+                    $(".counter").text(140);
+                }
             })
-    }
-  
-})
-
-// Load tweet function which executes a GET request to load tweets
-    loadTweets();
+        }
+    
+    })
 
 });
